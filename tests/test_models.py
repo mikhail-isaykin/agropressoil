23911d383208b7f20cbe011bb2866ba91783bd04
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-15 23:40:05 +0300

    test: verify products are sorted by order field within category

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,8 +1,19 @@
 from django.test import TestCase
-from catalog.models import Category
+from catalog.models import Category, Product
 
 
 class CategoryModelTest(TestCase):
     def test_slug_auto_generated(self):
         category = Category.objects.create(title='Тестовая категория')
         self.assertTrue(category.slug)
+
+def test_products_ordered_by_order_field(self):
+    category = Category.objects.create(title='Техника')
+    Product.objects.create(title='Продукт А', category=category, order=3)
+    Product.objects.create(title='Продукт Б', category=category, order=1)
+    Product.objects.create(title='Продукт В', category=category, order=2)
+
+    products = list(category.products.all())
+    self.assertEqual(products[0].title, 'Продукт Б')
+    self.assertEqual(products[1].title, 'Продукт В')
+    self.assertEqual(products[2].title, 'Продукт А')
