fbe90c9b3fe9de25904c6b132c904aa21b9db718
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-06-13 00:36:53 +0300

    chore: strip inherited skeleton to start clean prod build

--- news/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import NewsView
-
-
-urlpatterns = [
-    path('', NewsView.as_view(), name='news'),
-]
--- news/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import ListView
-from .models import News
-
-
-class NewsView(ListView):
-    model = News
-    template_name = 'news/news.html'
-    context_object_name = 'news'
