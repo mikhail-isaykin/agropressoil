e7268dec234f8c64fb9c5e818865129c1813fca1
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-08-03 23:55:05 +0300

    feat: wire up project URLs and serve media files in debug mode

--- config/urls.py
+++ config/urls.py
@@ -1,18 +1,13 @@
-from django.urls import path, include
-from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
-
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('catalog/', include('catalog.urls')),
-    path('repair/', TemplateView.as_view(template_name='repair/repair.html'), name='repair'),
+    path('', include('pages.urls')),
     path('news/', include('news.urls')),
-    path('about/', TemplateView.as_view(template_name='about/about.html'), name='about'),
-    path('contacts/', include('contacts.urls')),
+    path('catalog/', include('catalog.urls')),
 ]
 
 if settings.DEBUG:
