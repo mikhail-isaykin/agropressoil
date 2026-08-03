65845b0a7d6204fc07000076ab633ff2282ceafe
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-08-03 23:53:58 +0300

    feat: add news URL routes for list and detail views

---
+++ news/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'news'
+
+urlpatterns = [
+    path('', views.NewsListView.as_view(), name='news_list'),
+    path('<slug:slug>/', views.NewsDetailView.as_view(), name='news_detail'),
+]
